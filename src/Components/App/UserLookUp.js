0fb592c1c05eadc11faa6f413a46a2bb023a543a
Karen Garcia <karen.garcia@nexscreen.com>
2019-08-11 21:27:04 -0600

    working on being able to pull the friends from the list. Getting the id numbers but not able to associate those with names yet...yet

--- src/Components/App/UserLookUp.js
+++ src/Components/App/UserLookUp.js
@@ -2,33 +2,51 @@ import React, {useState} from 'react';
 import './App.css';
 import userData from '../../data/users';
 
+const ToDo = ({ userId, friends, index }) =>{
+  return(
+    <div 
+      className="todo"
+     >
+      {userData.index}
+      {userId.name}
+      {userData.userId}
+      Jello
+      {friends}
+      {index}
+
+    </div>
+  );
+ }
 
 const UserLookUp = () => {
   const [request, setRequest] = useState(userData[0].name);
-  const [userId, setUserID] = useState(0);
+  const [userId, setUserID] = useState(userData[0].id);
   const [address, setAddress] = useState(userData[0].address);
+  const [friends, setFriends] = useState(userData[0].friends);
 
+  console.log(userData[userId]["friends"]);
   const handleChange = event => {
     var y = event;
-    console.log({y});
-    console.log(y);
+
     if (y !== undefined){
       if (y > 0 && y < userData.length-1){
         var results = userData[y];
         var nameResults = results["name"];
         var address = results["address"];
+        var friends = results["friends"];
         setRequest(nameResults);
-        setAddress(address)
+        setAddress(address);
+        setFriends(friends);
       }else{
         setRequest(0);}
     };  
   };
 
   const userChange = event =>{
-    var x = event.target.value;
-    setUserID(event.target.value);
+    var x = event.target.value-1;
+    var variable = userData[x].id;
+    setUserID(variable);
     handleChange(x);
-
   };
 
   return (
@@ -46,7 +64,18 @@ const UserLookUp = () => {
       <section>
         Contact information {address}
       </section>
-      <div>
+      <section>
+        My friends are: {friends}
+      </section>
+      <div className="todo-list">
+      {friends.map((friends, index)=>(
+        <ToDo
+          key={index} 
+          friends={friends}
+          userId={userId}
+          index = {index}
+        />
+      ))}
 
       </div>
    </div>
