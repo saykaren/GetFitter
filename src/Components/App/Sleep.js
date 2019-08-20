ea0365a6a62f1dba700688480a0f93e1797284a3
Karen Garcia <karen.garcia@nexscreen.com>
2019-08-20 10:58:51 -0600

    added sleep details for a certain day that will update as the user updates

--- src/Components/App/Sleep.js
+++ src/Components/App/Sleep.js
@@ -1,17 +1,30 @@
-// import React, {useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import React from 'react';
+// import React from 'react';
 // import activityData from '../../data/activity';
 // import userData from '../../data/users';
+import sleepData from '../../data/sleep';
 
 const Sleep = ({Id, name})=>{
+
+  const sleepIdFilter = sleepData.filter(x=>(x.userID === Id));
+  const todaySleepID = sleepIdFilter.filter(x=>(x.date === "2019/09/22"));
+  const sleepToday = todaySleepID[0]["hoursSlept"];
+  // console.log({todaySleepID});
+  const goodSleep = parseInt(8-sleepToday);
 
   return(
     <div className="sleepInfo">
     <h1>Sleep Data</h1>
-    <article>
-      {name} sleep for ID {Id} is.....
-      You are under/over 8 hours....(calculate)
+    <article className="sleepDetails">
+      {name} you slept for {sleepToday} hours last night
+      <p>
+      You are under {goodSleep} hours
+      </p>
+      <p> 
+      of the 8 hours you need
+      </p>
+      
     </article>
   </div>
   )
